ec7c9f897825cf6e60f4f09853cd36817a854b28
sproul21 <88341959+sproul21@users.noreply.github.com>
2022-02-09 14:53:31 -0500

    Add files via upload

---
+++ Main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strings"
+)
+
+func wordCount(str string) map[string]int {
+	fileasBytes, err := ioutil.ReadFile(str)
+
+	fileContents := string(fileasBytes)
+
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	processedString := reg.ReplaceAllString(fileContents, " ")
+
+	words := strings.Split(processedString, " ")
+
+	counts := make(map[string]int)
+
+	for _, word := range words {
+		_, ok := counts[word]
+		if ok {
+			counts[word] += 1
+		} else {
+			counts[word] = 1
+		}
+	}
+
+	return counts
+
+}
+func reportResults(str string) {
+	for index, element := range wordCount(str) {
+		fmt.Println(index, ":", element)
+
+	}
+
+}
+
+func main() {
+	var str string
+	fmt.Println("Enter File Name: ")
+	fmt.Scan(&str)
+	reportResults(str)
+
+}
